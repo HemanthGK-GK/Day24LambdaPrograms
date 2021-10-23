d0b2eb365a0ee12216fe3b10fa7effb152205d3a
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-23 15:20:27 +0530

    Hemanth :Validating Last Name of the user

---
+++ UC2RegExLastName/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UC2RegExLastName
+{
+    class Program
+    {
+        class ChkLastName
+        {
+            public string RegexLastName = "^[A-Z]{1}[a-z]{2,}$";
+            public bool Validate(string lastname)
+            {
+                return Regex.IsMatch(lastname, RegexLastName);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string lastName;
+            bool result;
+            ChkLastName obj = new ChkLastName();
+
+            Console.WriteLine("Enter LastName");
+            lastName = Console.ReadLine();
+
+
+            result = obj.Validate(lastName);
+            Console.WriteLine(result);
+        }
+    }
+}
